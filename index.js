546195b80a763ecd877e2449591537fcd8229be5
JacquelineHockin <jacquelinehcashman@gmail.com>
2023-01-10 18:39:54 -1000

    building engineer prompt

--- index.js
+++ index.js
@@ -108,34 +108,84 @@ const promptMenu = () => {
 };
 
 // Add new engineer
-const promptEngineer = [
-  {
-    name: "name",
-    type: "input",
-    message: "What is the name of the engineer?",
-  },
-  {
-    name: "id",
-    type: "input",
-    message: "What is the ID of the engineer?",
-  },
-  {
-    name: "email",
-    type: "input",
-    message: "What is the engineer's email address?",
-  },
-  {
-    name: "github",
-    type: "input",
-    message: "What is the engineer's Github username?",
-  },
-  {
-    name: "upNext",
-    type: "list",
-    choices: ["Add Engineer", "Add Intern", "My team is complete!"],
-    message: "What next?",
-  },
-];
+const promptEngineer = () => {
+  console.log(`
+  ================
+  Add a New Engineer
+  ================
+  `);
+
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the name of the engineer?",
+        validate: (engineerName) => {
+          if (engineerName) {
+            return true;
+          } else {
+            console.log("Please enter the engineer's name to continue");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "engineerId",
+        message: "What is the ID of the engineer?",
+        validate: (engineerId) => {
+          if (engineerId) {
+            return true;
+          } else {
+            console.log("Please enter the engineer's Id to continue");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the engineer's email address?",
+        validate: (email) => {
+          if (email) {
+            return true;
+          } else {
+            console.log(
+              "Please enter the engineer's email address to continue"
+            );
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "What is the engineer's Github username?",
+        validate: (github) => {
+          if (github) {
+            return true;
+          } else {
+            console.log(
+              "Please enter the engineer's Github username to continue"
+            );
+            return false;
+          }
+        },
+      },
+    ])
+    .then((answers) => {
+      console.log(answers);
+      const engineer = new Engineer(
+        answers.name,
+        answers.engineerId,
+        answers.email,
+        answers.github
+      );
+      team.push(engineer);
+      promptMenu();
+    });
+};
 
 // Add intern
 const promptIntern = [
