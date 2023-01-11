46d9d65c558cdd857c0a7e4a51e731680636e596
JacquelineHockin <jacquelinehcashman@gmail.com>
2023-01-10 19:47:05 -1000

    reformatted index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
-const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
+const Intern = require("./lib/Intern");
 const Engineer = require("./lib/Engineer");
 const generateTeam = require("./src/generate-page");
 const fs = require("fs");
@@ -10,7 +10,6 @@ const DIST_DIR = path.resolve(__dirname, "dist");
 const distPath = path.join(DIST_DIR, "team.html");
 
 const team = [];
-const idArray = [];
 
 const promptManager = () => {
   return inquirer
@@ -101,9 +100,84 @@ const promptMenu = () => {
           console.log("Add Engineer");
           promptEngineer();
           break;
-        default:
-          buildTeam(team);
+        // default:
+        //   buildTeam(team);
       }
+    });
+};
+
+// Add intern
+const promptIntern = () => {
+  console.log(`
+  ================
+  Add a New Intern
+  ================
+  `);
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Name of intern? (Required):",
+        validate: (internName) => {
+          if (internName) {
+            return true;
+          } else {
+            console.log("Please enter interns name to proceed!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "employeeId",
+        message: "Enter employees ID number (Required):",
+        validate: (employeeId) => {
+          if (employeeId) {
+            return true;
+          } else {
+            console.log("Please enter employee ID to proceed!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Please enter email (Required):",
+        validate: email => {
+          if (email) {
+            return true;
+          } else {
+            console.log("Please enter email to proceed!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "Name of interns school (Required):",
+        validate: school => {
+          if (school) {
+            return true;
+          } else {
+            console.log("Please enter interns school to proceed!");
+            return false;
+          }
+        },
+      },
+    ])
+    .then(answers => {
+      console.log(answers);
+      const intern = new Intern(
+        answers.name,
+        answers.employeeId,
+        answers.email,
+        answers.school
+      );
+      team.push(intern);
+      promptMenu();
     });
 };
 
@@ -121,7 +195,7 @@ const promptEngineer = () => {
         type: "input",
         name: "name",
         message: "What is the name of the engineer?",
-        validate: (engineerName) => {
+        validate: engineerName => {
           if (engineerName) {
             return true;
           } else {
@@ -134,7 +208,7 @@ const promptEngineer = () => {
         type: "input",
         name: "engineerId",
         message: "What is the ID of the engineer?",
-        validate: (engineerId) => {
+        validate: engineerId => {
           if (engineerId) {
             return true;
           } else {
@@ -147,7 +221,7 @@ const promptEngineer = () => {
         type: "input",
         name: "email",
         message: "What is the engineer's email address?",
-        validate: (email) => {
+        validate: email => {
           if (email) {
             return true;
           } else {
@@ -162,7 +236,7 @@ const promptEngineer = () => {
         type: "input",
         name: "github",
         message: "What is the engineer's Github username?",
-        validate: (github) => {
+        validate: github => {
           if (github) {
             return true;
           } else {
@@ -174,7 +248,7 @@ const promptEngineer = () => {
         },
       },
     ])
-    .then((answers) => {
+    .then(answers => {
       console.log(answers);
       const engineer = new Engineer(
         answers.name,
@@ -186,35 +260,5 @@ const promptEngineer = () => {
       promptMenu();
     });
 };
-
-// Add intern
-const promptIntern = [
-  {
-    name: "name",
-    type: "input",
-    message: "What is the intern's name?",
-  },
-  {
-    name: "id",
-    type: "input",
-    message: "What is the intern's employee ID?",
-  },
-  {
-    name: "email",
-    type: "input",
-    message: "What is the intern's email address?",
-  },
-  {
-    name: "school",
-    type: "input",
-    message: "What college/university does the intern go to?",
-  },
-  {
-    name: "upNext",
-    type: "list",
-    choices: ["Add Engineer", "Add Intern", "My team is complete!"],
-    message: "What next?",
-  },
-];
 
 promptManager();
